a026507f62d2bf79a19a7ae030c9598dcdc50887
shawniwe <masa29035@gmail.com>
2024-04-18 16:41:52 +0500

    DefaultAreaCalculator перенесен на domain слой

---
+++ src/Mindbox.Geometry.Domain/DefaultAreaCalculator.cs
@@ -0,0 +1,9 @@
+﻿using Mindbox.Geometry.Abstract;
+
+namespace Mindbox.Geometry.Domain
+{
+    public class DefaultAreaCalculator : IAreaCalculator
+    {
+        public double CalculateArea(IFigure figure) => figure.CalculateArea();
+    }
+}
